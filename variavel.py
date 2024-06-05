0e95010222496a95093117d9c77aa603caa00950
MarceloA1ves01 <marceloalvesyz@gmail.com>
2024-06-05 14:43:29 -0300

    commit

---
+++ variavel.py
@@ -0,0 +1,8 @@
+#Aprendendo a utilizar variaveis
+nome = "marcelo"
+idade = 16
+altura= 1.75
+maior_de_idade = idade>=18
+print("Nome:", nome,"Idade:",idade,"Altura:",altura)
+print("é maior de idade ?", maior_de_idade)
+
